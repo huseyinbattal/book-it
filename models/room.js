6cda6804cb5dc901b47b6459e2c7aacffbbb4f11
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-21 23:39:04 +0300

    add deleteMany() and insertMany() methods

--- models/room.js
+++ models/room.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const roomSchema = new mongoose.Schema({
   name: {
@@ -108,5 +108,4 @@ const roomSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-
-export default mongoose.models.Room || mongoose.model("Room", roomSchema);
+module.exports = mongoose.models.Room || mongoose.model("Room", roomSchema);
--- utils/seeder.js
+++ utils/seeder.js
@@ -1,4 +1,26 @@
-import Room from "../models/room";
-import mongoose from "mongoose";
-import rooms from "../data/rooms.json"
-import dbConnect from "../config/dbConnect"
+const Room = require("../models/room");
+const rooms = require("../data/rooms.json");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/bookit", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useFindAndModify: false,
+  //useCreateIndex: true,
+});
+
+const seedRooms = async () => {
+  try {
+    await Room.deleteMany();
+    console.log("Rooms are deleted");
+
+    await Room.insertMany(rooms);
+    console.log("All rooms are added.");
+    process.exit();
+  } catch (error) {
+    console.log(error.message);
+    process.exit();
+  }
+};
+
+seedRooms();
