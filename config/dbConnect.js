23fb930588db5e5ce6d113e3a5f89c076a12a81a
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-21 16:52:20 +0300

    npm i mongoose

---
+++ config/dbConnect.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const dbConnect = () => {
+  if (mongoose.connection.readyState >= 1) {
+    return console.log("No connection!");
+  }
+
+  mongoose
+    .connect(process.env.DB_LOCAL_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
+    .then((con) => console.log("Connected to local database."));
+};
+
+export default dbConnect;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports={
+    env: {
+        DB_LOCAL_URI:"mongodb://localhost:27017/bookit"
+    }
 }
-
-module.exports = nextConfig
