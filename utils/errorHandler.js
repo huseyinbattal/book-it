c7e311bd73ebdb3c11d7cf7f40c98f09ea62ab6a
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-22 22:31:09 +0300

    add errorHandling

---
+++ utils/errorHandler.js
@@ -0,0 +1,7 @@
+class ErrorHandler extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
