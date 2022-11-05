98466abfea6fdc4508e1ac63972f74b0b53978db
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-05 18:17:53 +0300

    define reducers and constants

--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import { allRoomsReducer } from "./roomReducers";
 
 const reducer = combineReducers({
-    
-})
+  allRooms: allRoomsReducer,
+});
 
 export default reducer;
---
+++ redux/reducers/roomReducers.js
@@ -0,0 +1,25 @@
+import { ALL_ROOMS_SUCCESS, ALL_ROOMS_FAIL, CLEAR_ERRORS } from "../constants/roomConstants";
+
+// All room reducer
+export const allRoomsReducer = (state = { rooms: [] }, action) => {
+  switch (action.type) {
+    case ALL_ROOMS_SUCCESS:
+      return {
+        roomsCount: action.payload.roomsCount,
+        resPerPage: action.payload.resPerPage,
+          filteredRoomsCount: action.payload.filteredRoomsCount,
+        rooms:action.payload.rooms
+      };
+    case ALL_ROOMS_FAIL:
+          return {
+          error:action.payload
+          };
+      case CLEAR_ERRORS:
+          return {
+              error:null
+          }
+
+    default:
+      return state;
+  }
+};
