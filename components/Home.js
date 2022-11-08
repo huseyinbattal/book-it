e14a321fb35de6995e9c0654779d14cc6fbd0b07
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-08 15:25:15 +0300

    add pagination

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import Pagination from "react-js-pagination";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { clearErrors } from "../redux/actions/roomActions";
@@ -6,34 +8,63 @@ import RoomItem from "./room/RoomItem";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
 
-  const { rooms, error } = useSelector((state) => state.allRooms);
+  const { rooms, resPerPage, roomsCount, filteredRoomsCount, error } =
+    useSelector((state) => state.allRooms);
+
+  let { page = 1 } = router.query;
+  page = Number(page);
 
   useEffect(() => {
-   if (error) {
-     toast.error(error);
-     dispatch(clearErrors())
-   }
+    if (error) {
+      toast.error(error);
+      dispatch(clearErrors());
+    }
   }, []);
 
-  return (
-    <section id="rooms" className="container mt-5">
-      <h2 className="mb-3 ml-2 stays-heading">Stays in New York</h2>
+  const handlePagination = (pageNumber) => {
+    router.push(`/?page=${pageNumber}`);
+  };
 
-      <a href="#" className="ml-2 back-to-search">
-        {" "}
-        <i className="fa fa-arrow-left"></i> Back to Search
-      </a>
-      <div className="row">
-        {rooms && rooms.length === 0 ? (
-          <div className="alert alert-danger">
-            <b>No Rooms.</b>
-          </div>
-        ) : (
-          rooms && rooms.map((room) => <RoomItem key={room._id} room={room} />)
-        )}
-      </div>
-    </section>
+  return (
+    <>
+      <section id="rooms" className="container mt-5">
+        <h2 className="mb-3 ml-2 stays-heading">Stays in New York</h2>
+
+        <a href="#" className="ml-2 back-to-search">
+          {" "}
+          <i className="fa fa-arrow-left"></i> Back to Search
+        </a>
+        <div className="row">
+          {rooms && rooms.length === 0 ? (
+            <div className="alert alert-danger">
+              <b>No Rooms.</b>
+            </div>
+          ) : (
+            rooms &&
+            rooms.map((room) => <RoomItem key={room._id} room={room} />)
+          )}
+        </div>
+      </section>
+
+      {resPerPage < roomsCount && (
+        <div className="d-flex justify-content-center mt-5">
+          <Pagination
+            activePage={page}
+            itemsCountPerPage={resPerPage}
+            totalItemsCount={roomsCount}
+            onChange={handlePagination}
+            nextPageText="Next"
+            prevPageText="Prev"
+            firstPageText="First"
+            lastPageText="Last"
+            itemClass="page-item"
+            linkClass="page-link"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
