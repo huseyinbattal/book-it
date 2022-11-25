8c562a125126047084485f77a6a627eb513f935e
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-26 00:04:06 +0300

    fix bug with dropdown menu on header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadUser } from "../../redux/actions/userActions";
-import {signOut} from "next-auth/react"
+import { signOut } from "next-auth/react";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -15,7 +16,7 @@ const Header = () => {
 
   const logoutHandler = () => {
     signOut();
-  }
+  };
 
   return (
     <nav className="navbar row justify-content-center sticky-top">
@@ -35,38 +36,72 @@ const Header = () => {
 
         <div className="col-3 mt-3 mt-md-0 text-center">
           {user ? (
-            <div className="ml-4 dropdown d-line">
-            <a
-                className="btn dropdown-toggle mr-4"
-                id="dropDownMenuButton"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-            >
-                <figure className="avatar avatar-nav">
+            // <div className="ml-4 dropdown d-line">
+            //   <a
+            //     className="btn dropdown-toggle mr-4"
+            //     id="dropDownMenuButton"
+            //     data-toggle="dropdown"
+            //     aria-haspopup="true"
+            //     aria-expanded="false"
+            //   >
+            //     <figure className="avatar avatar-nav">
+            //       <img
+            //         src={user.avatar && user.avatar.url}
+            //         alt={user && user.name}
+            //         className="rounded-circle"
+            //       />
+            //     </figure>
+            //     <span>{user && user.name}</span>
+            //   </a>
+            //   <div
+            //     className="dropdown-menu"
+            //     aria-labelledby="dropDownMenuButton"
+            //   >
+            //     <Link href="/bookings/me">
+            //       <a className="dropdown-item">My Bookings</a>
+            //     </Link>
+            //     <Link href="/me/update">
+            //       <a className="dropdown-item">Profile</a>
+            //     </Link>
+            //     <Link href="/">
+            //       <a
+            //         onClick={logoutHandler}
+            //         className="dropdown-item text-danger"
+            //       >
+            //         Logout
+            //       </a>
+            //     </Link>
+            //   </div>
+            // </div>
+            <Dropdown>
+      <Dropdown.Toggle  variant="white" id="dropdown-basic">
+      <figure className="avatar avatar-nav">
                   <img
-                    src={user.avatar && user.avatar.url}
-                    alt={user && user.name}
-                    className="rounded-circle"
-                  />
-                </figure>
-                <span>{user && user.name}</span>
-              </a>
-              <div
-                className="dropdown-menu"
-                aria-labelledby="dropDownMenuButton"
-              >
-                <Link href="/bookings/me">
-                  <a className="dropdown-item">My Bookings</a>
-                </Link>
-                <Link href="/me/update">
-                  <a className="dropdown-item">Profile</a>
-                </Link>
-                <Link href="/">
-                  <a onClick={logoutHandler} className="dropdown-item text-danger">Logout</a>
-                </Link>
-              </div>
-            </div>
+                     src={user.avatar && user.avatar.url}
+                     alt={user && user.name}
+                     className="rounded-circle"
+                   />
+               </figure>
+           <span>{user && user.name}</span>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+      <Link href="/bookings/me">
+                   <a className="dropdown-item">My Bookings</a>
+                 </Link>
+                 <Link href="/me/update">
+                   <a className="dropdown-item">Profile</a>
+                 </Link>
+                 <Link href="/">
+                   <a
+                     onClick={logoutHandler}
+                     className="dropdown-item text-danger"
+                   >
+                     Logout
+                   </a>
+                 </Link>
+      </Dropdown.Menu>
+    </Dropdown>
           ) : (
             !loading && (
               <Link href="/login">
