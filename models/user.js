2ee15b72866f3516330977eabe736c1580d63beb
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-29 19:07:20 +0300

    Generate forget password token.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,18 +49,34 @@ const userSchema = new mongoose.Schema({
 });
 
 // Encrypting password before saving user
-
-userSchema.pre('save', async function (next) {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    next()
+    next();
   } else {
     this.password = await bcrypt.hash(this.password, 10);
   }
-})
+});
 
 // Compare user password
 userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword,this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash("sha258")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set token expire time
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+  return resetToken;
+};
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
