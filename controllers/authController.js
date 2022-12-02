ed5011da1971b0deea6da1bb4a6c37a81c781ce1
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-02 14:10:14 +0300

    handle new password endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,9 @@ import User from "../models/user";
 import cloudinary from "cloudinary";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors";
-import absoluteUrl from 'next-absolute-url'
-import sendEmail from "../utils/sendEmail"
-
+import absoluteUrl from "next-absolute-url";
+import sendEmail from "../utils/sendEmail";
+import crypto from "crypto";
 
 // Setting up cloudinary config
 cloudinary.config({
@@ -90,7 +90,6 @@ const updateProfile = catchAsyncErrors(async (req, res) => {
 const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
 
-
   if (!user) {
     return next(new ErrorHandler("User not found with this email", 404));
   }
@@ -100,37 +99,81 @@ const forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-      // Get origin
-      const { origin } = absoluteUrl(req)
+  // Get origin
+  const { origin } = absoluteUrl(req);
 
   //Create reset password url
-  const resetUrl = `${origin}/password/reset/${resetToken}`
+  const resetUrl = `${origin}/password/reset/${resetToken}`;
 
-  const message = `Your password reset url is as follow: \n\n ${resetUrl} \n\n\ If you have not requested this email, then ignore it.`
+  const message = `Your password reset url is as follow: \n\n ${resetUrl} \n\n\ If you have not requested this email, then ignore it.`;
 
   try {
     await sendEmail({
-        email: user.email,
-        subject: 'BookIT Password Recovery',
-        message
-    })
+      email: user.email,
+      subject: "BookIT Password Recovery",
+      message,
+    });
 
     res.status(200).json({
-        success: true,
-        message: `Email sent to: ${user.email}`
-    })
-
-
-} catch (error) {
+      success: true,
+      message: `Email sent to: ${user.email}`,
+    });
+  } catch (error) {
     user.resetPasswordToken = undefined;
     user.resetPasswordExpire = undefined;
 
-    await user.save({ validateBeforeSave: false })
+    await user.save({ validateBeforeSave: false });
 
-    return next(new ErrorHandler(error.message, 500))
+    return next(new ErrorHandler(error.message, 500));
   }
-  
-
 });
 
-export { registerUser, currentUserProfile, updateProfile, forgotPassword };
+// Reset password    =>    /api/password/reset/:token
+const resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+
+//Hash URL token
+  const resetPasswordToken = crypto.createHash("sha256").update(req.query.token).digest("hex")
+  
+  
+
+  const user = await User.findOne({
+
+    resetPasswordToken,
+    resetPasswordExpire:{ $gt:Date.now() }
+
+   });
+
+  if (!user) {
+    return next(new ErrorHandler("Password reset token is invalid or has been expired", 400));
+  }
+
+if (req.body.password!==req.body.confirmPassword) {
+  return next(new ErrorHandler("Password does not match", 400));
+}
+  
+  // Setup the new password
+  user.password = req.body.password
+  
+  user.resetPasswordToken = undefined
+  
+  user.resetPasswordExpire=undefined
+  
+  await user.save();
+
+
+  res.status(200).json({
+    success: true,
+    message:"Password updated successfully",
+  })
+  
+  
+});
+
+export {
+  registerUser,
+  currentUserProfile,
+  updateProfile,
+  forgotPassword,
+  resetPassword,
+};
