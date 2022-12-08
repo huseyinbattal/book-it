0cdd97bac69c0e263de1f9d2aba82588e6be0066
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-08 23:13:12 +0300

    install easyinvoice

--- components/booking/BookingDetails.js
+++ components/booking/BookingDetails.js
@@ -20,24 +20,35 @@ const BookingDetails = () => {
     <div className="container">
       <div className="row d-flex justify-content-between">
         <div className="col-12 col-lg-8 mt-5 booking-details">
-          {booking && (
+          {booking && booking.room && booking.user && (
             <>
-            <h2 className="my-5">Booking # {booking._id}</h2>
+              <h2 className="my-5">Booking # {booking._id}</h2>
               <h4 className="mb-4">User Info</h4>
-              <p><b>Name:</b>{booking.user && booking.user.name}</p>
-              <p><b>Email:</b>{booking.user && booking.user.email}</p>
-            <p><b>Amount:</b> ${ booking.amountPaid}</p>
+              <p>
+                <b>Name: </b>
+                {booking.user && booking.user.name}
+              </p>
+              <p>
+                <b>Email: </b>
+                {booking.user && booking.user.email}
+              </p>
+              <p>
+                <b>Amount: </b> ${booking.amountPaid}
+              </p>
+
               <hr />
 
               <h4 className="mb-4">Booking Info</h4>
               <p>
-                <b>Check In:</b> CHECK_IN_DATE
+                <b>Check In: </b>{" "}
+                {new Date(booking.checkInDate).toLocaleString("tr-TR")}
               </p>
               <p>
-                <b>Check Out:</b> CHECK_OUT_DATE
+                <b>Check Out: </b>{" "}
+                {new Date(booking.checkOutDate).toLocaleString("tr-TR")}
               </p>
               <p>
-                <b>Days of Stay:</b> 5
+                <b>Days of Stay: </b> {booking.daysOfStay}
               </p>
 
               <hr />
@@ -53,19 +64,26 @@ const BookingDetails = () => {
               <div className="cart-item my-1">
                 <div className="row my-5">
                   <div className="col-4 col-lg-2">
-                    <img src="" alt="" />
+                    <Image
+                      src={booking.room.images[0].url}
+                      alt={booking.room.name}
+                      height={45}
+                      width={65}
+                    />
                   </div>
 
                   <div className="col-5 col-lg-5">
-                    <a href="#">Room Name</a>
+                    <Link href={`/room/${booking.room._id}`}>
+                      {booking.room.name}
+                    </Link>
                   </div>
 
                   <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                    <p>$45</p>
+                    <p>${booking.room.pricePerNight}</p>
                   </div>
 
                   <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                    <p>4 Day(s)</p>
+                    <p>{booking.daysOfStay} Day(s)</p>
                   </div>
                 </div>
               </div>
