cecd3e78a25f5aed1ca3b913be29ab4808133072
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-11 18:23:36 +0300

    create webhook api

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -48,19 +48,53 @@ const stripeCheckoutSession = catchAsyncErrors(async (req, res) => {
 
 // Create new booking after payment    =>    /api/webhook
 const webhookCheckout = catchAsyncErrors(async (req, res) => {
+  const rawBody = await getRawBody(req);
 
- try {
-  
-   const signature = req.header["stripe-signature"];
+  try {
+    const signature = req.header["stripe-signature"];
 
-   const event=stripe.webhooks.constructEvent()
-   
-   
- } catch (error) {
-   
-   console.log("Error in Stripe Checkout Payment =>", error);
-   
- }
+    const event = stripe.webhooks.constructEvent(
+      rawBody,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      const room = session.client_reference_id;
+      const user = (await User.findOne({ email: session.customer_email })).id;
+
+      const amountPaid = session.amount_total / 100;
+
+      const paymentInfo = {
+        id: session.payment_intent,
+        status: session.payment_status,
+      };
+
+      const checkInDate = session.metadata.checkInDate;
+      const checkOutDate = session.metadata.checkOutDate;
+      const daysOfStay = session.metadata.daysOfStay;
+
+      const booking = await Booking.create({
+        room,
+        user,
+        checkInDate,
+        checkOutDate,
+        daysOfStay,
+        amountPaid,
+        paymentInfo,
+        paidAt: Date.now(),
+      });
+
+      res.status(200).json({ success: true });
+    }
+  } catch (error) {
+    console.log("Error in Stripe Checkout Payment =>", error);
+  }
 });
 
-export { stripeCheckoutSession };
+export {
+  stripeCheckoutSession,
+  webhookCheckout,
+};
