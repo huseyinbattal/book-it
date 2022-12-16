dcb34d0f3f2a315b8532c5c05c2f22e1f52061ee
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-16 18:18:21 +0300

    authorize roles

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,4 +13,19 @@ const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   next();
 });
 
-export { isAuthenticatedUser };
+// Handling user roles
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user.role}) is not allowed to access this resource! `,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
+
+export { isAuthenticatedUser, authorizeRoles };
--- pages/api/admin/rooms/index.js
+++ pages/api/admin/rooms/index.js
@@ -4,12 +4,14 @@ import dbConnect from "../../../../config/dbConnect";
 import { allAdminRooms } from "../../../../controllers/roomControllers";
 
 import onError from "../../../../middlewares/errors";
-import { isAuthenticatedUser } from "../../../../middlewares/auth";
+import { isAuthenticatedUser,authorizeRoles } from "../../../../middlewares/auth";
 
 const handler = nc({ onError });
 
 dbConnect();
 
-handler.use(isAuthenticatedUser).get(allAdminRooms);
+handler
+    .use(isAuthenticatedUser,authorizeRoles("admin"))
+    .get(allAdminRooms)
 
 export default handler;
