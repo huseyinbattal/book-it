897fc49f005041bf8ece176dfb1d0064909c8d1d
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-01 13:45:39 +0300

    update db connect uri

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -6,7 +6,7 @@ const dbConnect = () => {
   }
 
   mongoose
-    .connect(process.env.DB_LOCAL_URI, {
+    .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       //useFindAndModify: false,
