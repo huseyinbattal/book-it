be46c0b28187ec9b5b67b664814e38c75b811cc3
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-02 14:27:58 +0300

    update footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-1">
       <p className="text-center mt-1">
-        Book IT - 2019-2021, All Rights Reserved
+        Book IT - 2023, All Rights Reserved
       </p>
     </footer>
   );
