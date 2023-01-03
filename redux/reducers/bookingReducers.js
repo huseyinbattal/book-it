2ec235635956e5b667e59de63d5d903e32d9ee7d
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-03 14:10:32 +0300

    fix bug

--- redux/reducers/bookingReducers.js
+++ redux/reducers/bookingReducers.js
@@ -16,164 +16,170 @@ import {
   DELETE_BOOKING_SUCCESS,
   DELETE_BOOKING_RESET,
   DELETE_BOOKING_FAIL,
-  CLEAR_ERRORS,
-} from "../constants/bookingConstants";
 
-// Check Bookings reducer
+  CLEAR_ERRORS
+
+} from '../constants/bookingConstants'
+
+
+// Check Booking
 export const checkBookingReducer = (state = { available: null }, action) => {
   switch (action.type) {
-    case CHECK_BOOKING_REQUEST:
-      return {
-        loading: true,
-      };
 
-    case CHECK_BOOKING_SUCCESS:
-      return {
-        loading: false,
-        available: action.payload,
-      };
+      case CHECK_BOOKING_REQUEST:
+          return {
+              loading: true
+          }
 
-    case CHECK_BOOKING_RESET:
-      return {
-        loading: false,
-        available: null,
-      };
+      case CHECK_BOOKING_SUCCESS:
+          return {
+              loading: false,
+              available: action.payload
+          }
 
-    case CHECK_BOOKING_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
+      case CHECK_BOOKING_RESET:
+          return {
+              loading: false,
+              available: null
+          }
 
-    case CLEAR_ERRORS:
-      return {
-        ...state,
-        error: null,
-      };
+      case CHECK_BOOKING_FAIL:
+          return {
+              loading: false,
+              error: action.payload
+          }
 
-    default:
-      return state;
+      case CLEAR_ERRORS:
+          return {
+              ...state,
+              error: null
+          }
+
+      default:
+          return state
   }
-};
+}
 
-// Get all book dates reducer
+
+// Get all booked dates
 export const bookedDatesReducer = (state = { dates: [] }, action) => {
   switch (action.type) {
-    case BOOKED_DATES_SUCCESS:
-      return {
-        loading: false,
-        dates: action.payload,
-      };
+      case BOOKED_DATES_SUCCESS:
+          return {
+              loading: false,
+              dates: action.payload
+          }
 
-    case BOOKED_DATES_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
+      case BOOKED_DATES_FAIL:
+          return {
+              loading: false,
+              error: action.payload
+          }
 
-    case CLEAR_ERRORS:
-      return {
-        ...state,
-        error: null,
-      };
+      case CLEAR_ERRORS:
+          return {
+              ...state,
+              error: null
+          }
 
-    default:
-      return state;
+      default:
+          return state
   }
-};
+}
 
-// Bookings reducer
+
 export const bookingsReducer = (state = { bookings: [] }, action) => {
   switch (action.type) {
 
-    case ADMIN_BOOKINGS_REQUEST:
-      return {
-        loading: true
-      };
+      case ADMIN_BOOKINGS_REQUEST:
+          return {
+              loading: true,
+          }
 
-    case MY_BOOKINGS_SUCCESS:
-    case ADMIN_BOOKINGS_SUCCESS:
-      return {
-        loading: false,
-        bookings: action.payload,
-      };
+      case MY_BOOKINGS_SUCCESS:
+      case ADMIN_BOOKINGS_SUCCESS:
+          return {
+              loading: false,
+              bookings: action.payload
+          }
 
-    case MY_BOOKINGS_FAIL:
+      case MY_BOOKINGS_FAIL:
       case ADMIN_BOOKINGS_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
+          return {
+              loading: false,
+              error: action.payload
+          }
 
-    case CLEAR_ERRORS:
-      return {
-        ...state,
-        error: null,
-      };
+      case CLEAR_ERRORS:
+          return {
+              ...state,
+              error: null
+          }
 
-    default:
-      return state;
+      default:
+          return state
   }
-};
+}
 
 export const bookingDetailsReducer = (state = { booking: {} }, action) => {
   switch (action.type) {
-    case BOOKING_DETAILS_SUCCESS:
-      return {
-        loading: false,
-        booking: action.payload,
-      };
+      case BOOKING_DETAILS_SUCCESS:
+          return {
+              loading: false,
+              booking: action.payload
+          }
 
-    case BOOKING_DETAILS_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
+      case BOOKING_DETAILS_FAIL:
+          return {
+              loading: false,
+              error: action.payload
+          }
 
-    case CLEAR_ERRORS:
-      return {
-        ...state,
-        error: null,
-      };
+      case CLEAR_ERRORS:
+          return {
+              ...state,
+              error: null
+          }
 
-    default:
-      return state;
+      default:
+          return state
   }
-};
+}
 
-// Check Booking
-export const bookingReducer = (state = { }, action) => {
+
+export const bookingReducer = (state = {}, action) => {
   switch (action.type) {
-    case DELETE_BOOKING_REQUEST:
-      return {
-        loading: true,
-      };
 
-    case DELETE_BOOKING_SUCCESS:
-      return {
-        loading: false,
-        isDeleted: action.payload,
-      };
+      case DELETE_BOOKING_REQUEST:
+          return {
+              loading: true
+          }
 
-    case DELETE_BOOKING_RESET:
-      return {
-        loading: false,
-        isDeleted: false,
-      };
+      case DELETE_BOOKING_SUCCESS:
+          return {
+              loading: false,
+              isDeleted: action.payload
+          }
 
-    case DELETE_BOOKING_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
+      case DELETE_BOOKING_RESET:
+          return {
+              loading: false,
+              isDeleted: false
+          }
 
-    case CLEAR_ERRORS:
-      return {
-        ...state,
-        error: null,
-      };
+      case DELETE_BOOKING_FAIL:
+          return {
+              loading: false,
+              error: action.payload
+          }
 
-    default:
-      return state;
+      case CLEAR_ERRORS:
+          return {
+              ...state,
+              error: null
+          }
+
+      default:
+          return state
   }
-};
+}
